f15c9a8fc77ed42efe16229a0f173e069cf66983
Bhuvaneshwaran <bhuvaneshrm@gmail.com>
2023-05-12 15:21:49 +0530

    removed test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Products from './components/Products';
-import Test from "./components/Test"
+
 
 function App() {
 
